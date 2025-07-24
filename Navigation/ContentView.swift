5ba96c0e45a1b9cb4812235184375f20e0b9996f
Surya Singh-Prasad <surya.singhprasad@gmail.com>
2025-07-24 09:43:27 -0400

    Made Navigation stack + link to go between first and second view.

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack {
+                Text("HOME PAGE/ROOT PAGE √")
+                NavigationLink(destination: SecondView()) {
+                    Text("GO TO THE NEXT PAGE")
+                }
+            }
+        
+            .padding()
         }
-        .padding()
     }
 }
 
---
+++ Navigation/SecondView.swift
@@ -0,0 +1,14 @@
+//
+//  SecondView.swift
+//  Navigation
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import Foundation
+import SwiftUI
+struct SecondView: View {
+    var body: some View {
+        Text("SECONDARY PAGE 🥈")
+    }
+}
